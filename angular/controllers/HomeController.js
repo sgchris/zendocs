bbb2471002f3341b7b617406343127dced60fa27
Gregory Chris <sgchris@gmail.com>
2018-12-12 16:29:12 +0200

    displaying posts (with filters)

--- angular/controllers/HomeController.js
+++ angular/controllers/HomeController.js
@@ -1,22 +1,43 @@
-app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+app.controller('HomeController', ['$scope', '$http', 'UsersData', function($scope, $http, UsersData) {
     $scope.posts = {
-        resultsPerPage: 2,
+        resultsPerPage: 20,
+
+        usersData: {},
 
         data: [],
 
         init: function() {
-            console.log('init');
             var ref = POSTS
-                //.orderByChild('created_at')
+                .orderByChild('created_at')
                 .limitToLast($scope.posts.resultsPerPage);
             ref.once('value', function(snapshot) {
-                console.log('snapshot', snapshot.val());
-            });
-            POSTS.push({
-                title: 'title 2',
-                content: 'content 2',
-                created_at: Math.floor((new Date()).getTime() / 1000), // timestamp
-                user: 'sgchris@gmail.com'
+                var postsList = snapshot.val();
+                if (postsList) {
+                    Object.keys(postsList).forEach(function(postId) {
+                        var postData = postsList[postId];
+                        postsList[postId].postid = postId;
+
+                        // post's user ID
+                        var uid = postData.uid;
+                        
+                        // load users data
+                        if (!$scope.posts.usersData[uid]) {
+                            UsersData(uid, function(uData) {
+                                if (!$scope.posts.usersData[uid]) {
+                                    $scope.posts.usersData[uid] = uData;
+                                }
+                            });
+                        }
+                    });
+
+                    postsList = Object.values(postsList).reverse();
+                }
+
+                // set the data locally
+                $scope.$apply(function() {
+                    $scope.posts.data = postsList;
+                    console.log('$scope.posts.data', $scope.posts.data);
+                });
             });
         }
     }
---
+++ angular/services/filters.js
@@ -0,0 +1,41 @@
+app.filter('timestampToRelativeDate', [function() {
+    return function(ts) {
+        // check if ts is in milliseconds
+        if (('' + ts).length > 10) {
+            ts = Math.floor(ts / 1000);
+        }
+
+        var currentTimestamp = Math.floor((new Date()).getTime() / 1000);
+        var period = currentTimestamp - ts;
+
+        var days = parseInt(period / (3600 * 24));
+        period -= (days * 3600 * 24);
+        var hours = parseInt(period / 3600);
+        period -= (hours * 3600);
+        var minutes = parseInt(period / (60));
+
+        var timeElements = [];
+        if (days > 0) {
+            timeElements.push(days + ' day' + (days != 1 ? 's' : ''));
+        }
+        if (hours > 0) {
+            timeElements.push(hours + ' hour' + (hours != 1 ? 's' : ''));
+        }
+        if (minutes > 0) {
+            timeElements.push(minutes + ' minute' + (minutes != 1 ? 's' : ''));
+        }
+        if (timeElements.length === 0) {
+            timeElements.push('less than a minute');
+        }
+
+        return timeElements.slice(0, 2).join(' and ') + ' ago';
+    };
+}]);
+
+
+app.filter('contentPreview', [function() {
+    return function(content) {
+        // just reluctantly cut the text
+        return content.substr(0, 500); 
+    }
+}]);
